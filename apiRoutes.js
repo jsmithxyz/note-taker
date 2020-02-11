1f01017d3446f4fe4b01dddd97e4431c99e70371
jsmithxyz <jordansmith3813@gmail.com>
2020-02-11 14:11:12 -0500

    created GET and POST for notes

---
+++ apiRoutes.js
@@ -0,0 +1,23 @@
+
+
+var fs = require("fs");
+var path = require("path");
+
+module.exports = function(app) {
+
+  app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./db/db.json"));
+});
+
+  app.post("/api/notes", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let newNote = req.body;
+    savedNotes.push(newNote);
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    console.log("Note saved to db.json. Content: ", newNote);
+    res.json(savedNotes);
+});
+}
+
+  
