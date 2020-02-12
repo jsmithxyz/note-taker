e440a3fd504750795beaf37f59a4ded57edc4bd5
jsmithxyz <jordansmith3813@gmail.com>
2020-02-12 14:23:19 -0500

    completed DELETE method functionality

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var fs = require("fs");
 
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
